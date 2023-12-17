c832c7060348453880c1e9ecfbd30a41d1b95612
hemanth3509 <saihemanth2230@gmail.com>
2023-12-17 12:02:22 +0530

    Mongodb script 2nd version

--- mongodb.sh
+++ mongodb.sh
@@ -6,10 +6,23 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "Scripting Started executing at $TIMESTAMP " &>> $LOGFILE 
 
+check (){
+   if [ $1 -ne 0 ]
+   then
+        echo "$2...FAILED"
+        exit 1
+   else 
+        echo "$2... Success"
+   fi
+}
+
 if [ $ID -ne 0 ]
 then
 echo "Please run the script with root access"
 exit 1
 else 
-echo " Script running with root access"
+echo " Script running with root access" &>> $LOGFILE
 fi
+
+cp  mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+ check $? "Copying mongodb repo" 
