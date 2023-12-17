f6d880dade611f50d452d20bf09b15364f88af9a
hemanth3509 <saihemanth2230@gmail.com>
2023-12-17 12:08:06 +0530

    Mongodb script 2nd version

--- mongodb.sh
+++ mongodb.sh
@@ -26,3 +26,21 @@ fi
 
 cp  mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
  check $? "Copying mongodb repo" 
+
+ dnf install mongodb-org -y &>> $LOGFILE
+ check $? "Installing mongodb"
+
+ systemctl enable mongod &>> $LOGFILE
+ check $? "Enabling mongodb service"
+
+ systemctl start mongod &>> $LOGFILE
+ check $? "Starting mongodb service"
+
+ sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+ check $? "Editing mongod.conf file to provide remote access"
+ 
+ systemctl restart mongod &>> $LOGFILE
+ check $? "Restarting the  mongdb service"
+
+ systemctl status mongod
+ 
