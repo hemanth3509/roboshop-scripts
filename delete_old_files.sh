86ef62fba124883fea9abf0b71b35577c1f0e561
hemanth3509 <saihemanth2230@gmail.com>
2023-12-18 18:39:21 +0530

    robo shell script

---
+++ delete_old_files.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+
+#R="\e[31m"
+#G="\e[32m"
+#Y="\e[33m"
+#N="\e[0m"
+
+while getopts ":saDdt:" opt;
+do
+case $opt in
+s) SOURCE_DIR=$1 ; echo "source dir $SOURCE_DIR ";;
+a) echo " archive ";;
+D) echo "Delete " ;;
+d) DATE=$2 ; echo " date $DATE" ;;
+t) TIME=$3 ; echo " time is $TIME" ;;
+\?) help;exit 1 ;;
+esac
+done
+
+help(){
+    echo -e " Please find the below usage of the script \n"
+    echo -e " -s <source dir> \n "
+    echo -e " -a <Archive> \n "
+    echo -e " -D <Delete> \n "
+    echo -e " -d <destination > \n "
+    echo -e " -t <time> \n "
+}
+
+
+#if [ ! -d $SOURCE_DIR ] # ! denotes opposite
+#then
+#    echo -e "$R Source directory: $SOURCE_DIR does not exists. $N"
+#fi
+
+#FILES_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
+
+#while IFS= read -r line
+#do
+#    echo "Deleting file: $line"
+#    rm -rf $line
+#done <<< $FILES_TO_DELETE
